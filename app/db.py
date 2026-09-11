1bdbd94198b6c1af0419715f11d3f2bd45a7154c
SENSible2547 <160507188+SENSible2547@users.noreply.github.com>
2026-09-11 06:27:45 -0500

    Update db.py

--- app/db.py
+++ app/db.py
@@ -2,13 +2,16 @@ import psycopg2
 import psycopg2.extras
 
 DB_CONFIG = dict(
-    host="localhost",
+    host="database-3.cqyakzg5wwqq.us-east-1.rds.amazonaws.com",  # el endpoint de tu instancia RDS
     dbname="airline_reservations",
     user="postgres",
-    password="postgres",
+    password="o4oh1647t",
 )
 
-
 def get_connection():
-    conn = psycopg2.connect(**DB_CONFIG)
+    conn = psycopg2.connect(host="database-3.cqyakzg5wwqq.us-east-1.rds.amazonaws.com", dbname="airline_reservations",
+        user="postgres",
+        password="o4oh1647t")
     return conn
+
+
